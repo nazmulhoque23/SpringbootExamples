b9457118491b8fd5af13c9fd1b8d17ee628fb30c
nazmulhoque23 <hoque23kazi@gmail.com>
2022-07-25 12:30:39 +0600

    "DTO commit"

--- demoTutorialMappingApp/src/main/java/com/example/demotutorialmappingapp/service/UserService.java
+++ demoTutorialMappingApp/src/main/java/com/example/demotutorialmappingapp/service/UserService.java
@@ -1,4 +1,12 @@
 package com.example.demotutorialmappingapp.service;
 
-public interface UserService {
+import com.example.demotutorialmappingapp.dto.UserRegistrationDto;
+import com.example.demotutorialmappingapp.entity.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+    UserEntity save(UserRegistrationDto registrationDto);
+    List<UserEntity> getAll();
 }
