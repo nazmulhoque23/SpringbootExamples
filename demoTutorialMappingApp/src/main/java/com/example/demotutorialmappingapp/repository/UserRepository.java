4502c8658060e318df8b16bd2994dad07790effd
nazmulhoque23 <hoque23kazi@gmail.com>
2022-07-25 11:38:39 +0600

    "service commit"

--- demoTutorialMappingApp/src/main/java/com/example/demotutorialmappingapp/repository/UserRepository.java
+++ demoTutorialMappingApp/src/main/java/com/example/demotutorialmappingapp/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.demotutorialmappingapp.repository;
 
-public interface UserRepository {
+import com.example.demotutorialmappingapp.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
---
+++ demoTutorialMappingApp/src/main/java/com/example/demotutorialmappingapp/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.demotutorialmappingapp.service;
+
+public class UserService {
+}
